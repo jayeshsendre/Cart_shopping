00ee540bb2712cf404c3dfcc66bdaa9847b8c567
jayeshsendre <jayeshsendre20@gmail.com>
2023-06-14 23:42:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import "./App.css";
+import Footer from "./containers/Footer"
+import ProductDetails from "./containers/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
